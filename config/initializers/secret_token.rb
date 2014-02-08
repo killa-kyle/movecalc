bda3b775c06a9d536881c2e6196848b3acb566f4
Killa Kyle <kylerose@gmail.com>
2014-02-08 14:50:25 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Movecalc::Application.config.secret_key_base = 'b9820a79c32ce8d4fd8af496400c9811397aefe80a55d26c6c870884f0b7ce5a194ea6ff9229266f727a2b9677898c727bfbc1e4a5b01d3ad34566652609a9e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Movecalc::Application.config.session_store :cookie_store, key: '_movecalc_session'
